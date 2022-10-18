3a5b44fa5f0e4334a22096320bcfe1d16966e474
P8TF <saritpanta@VivSoftdminsMBP.attlocal.net>
2022-10-18 09:55:33 -0400

    space issue

--- main.tf
+++ main.tf
@@ -1,7 +1,6 @@
 data "aws_eks_cluster_auth" "cluster" {
   name = var.cluster_name
 }
-
 provider "helm" {
   kubernetes {
     host                   = var.cluster_endpoint
@@ -9,7 +8,6 @@ provider "helm" {
     token                  = data.aws_eks_cluster_auth.cluster.token
   }
 }
-
 provider "kubernetes" {
   host                   = var.cluster_endpoint
   cluster_ca_certificate = base64decode(var.cluster_certificate_authority_data)
