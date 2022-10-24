95e6a69367a50a1241d048c321dbe4f4c1c674e7
P8TF <saritpanta@ip-10-232-43-53.ec2.internal>
2022-10-24 09:57:09 -0400

    efs resource added

---
+++ efs.tf
@@ -0,0 +1,26 @@
+resource "aws_security_group" "efs" {
+  description = "${var.cluster_name} EFS"
+  vpc_id      = var.vpc_id
+}
+
+data "aws_kms_key" "efs" {
+  key_id = var.kms_key_id
+}
+
+resource "aws_efs_file_system" "efs" {
+  creation_token = var.cluster_name
+
+  encrypted  = true
+  kms_key_id = data.aws_kms_key.efs.arn
+
+  tags = {
+    Name = "${var.cluster_name}"
+  }
+}
+
+resource "aws_efs_mount_target" "efs" {
+  for_each        = toset(var.private_subnet_ids)
+  file_system_id  = aws_efs_file_system.efs.id
+  subnet_id       = each.key
+  security_groups = [aws_security_group.efs.id]
+}
